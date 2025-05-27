c5daf5f525b885268b71b12fcce9f9704f7b464b
unknown <muhammedkemal.57@gmail.com>
2025-05-27 13:33:19 +0300

    Added Dto and NoteController

---
+++ Server/NoteKeeperAPI/Core/NoteKeeperAPI.Application/DTO/Notes/CreateNoteDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteKeeperAPI.Application.DTO.Notes
+{
+    public class CreateNoteDto
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
