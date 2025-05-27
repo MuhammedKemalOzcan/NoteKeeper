bcbf07b52503b5d252d78dc1ca24369966715b7f
unknown <muhammedkemal.57@gmail.com>
2025-05-28 01:30:35 +0300

    added router and main layout

--- Client/NoteKeeperClient/src/App.tsx
+++ Client/NoteKeeperClient/src/App.tsx
@@ -1,14 +1,28 @@
-import { useState } from "react";
-import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import MainLayout from "./layouts/MainLayout";
+import AllNotes from "./pages/AllNotes";
+import Search from "./pages/Search";
+import ArchivedNotes from "./pages/ArchivedNotes";
+import Tags from "./pages/Tags";
+import Settings from "./pages/Settings";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      { index: true, element: <AllNotes /> },
+      { path: "home", element: <AllNotes /> },
+      { path: "search", element: <Search /> },
+      { path: "archived", element: <ArchivedNotes /> },
+      { path: "tags", element: <Tags /> },
+      { path: "settings", element: <Settings /> },
+    ],
+  },
+]);
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ Client/NoteKeeperClient/src/components/NavbarMobile.tsx
@@ -0,0 +1,40 @@
+import {
+  House,
+  LucideArchive,
+  Search,
+  SettingsIcon,
+  TagIcon,
+} from "lucide-react";
+import { NavLink } from "react-router";
+
+const links = [
+  { title: "Home", to: "/", icon: House },
+  { title: "Search", to: "/search", icon: Search },
+  { title: "Archived", to: "/archived", icon: LucideArchive },
+  { title: "Tags", to: "/tags", icon: TagIcon },
+  { title: "Settings", to: "/settings", icon: SettingsIcon },
+];
+
+export default function NavbarMobile() {
+  return (
+      <div className="fixed bottom-0 w-full h-20 flex justify-around items-center border-t p-3 ">
+        {links.map((link) => {
+          const Icon = link.icon;
+          return (
+            <NavLink
+              className={({ isActive }) =>
+                isActive
+                  ? " w-[25%] h-[80%] bg-blue-200 flex flex-col items-center justify-center rounded-[4px]"
+                  : " w-[25%] h-[80%] flex flex-col items-center justify-center"
+              }
+              key={link.to}
+              to={link.to}
+            >
+              <Icon color="black" size={24} />
+              <p className="max-sm:hidden">{link.title}</p>
+            </NavLink>
+          );
+        })}
+      </div>
+  );
+}
