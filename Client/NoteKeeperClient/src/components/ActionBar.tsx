7ef5bf9d8b3aa157cf2d411bd7349bd3edcdc838
unknown <muhammedkemal.57@gmail.com>
2025-06-04 18:33:38 +0300

    added action bar functionality

--- Client/NoteKeeperClient/src/components/ActionBar.tsx
+++ Client/NoteKeeperClient/src/components/ActionBar.tsx
@@ -1,21 +1,51 @@
-import { Archive, StepBack, Trash2 } from "lucide-react";
+import { Archive, RefreshCcw, StepBack, Trash2 } from "lucide-react";
+import { useNavigate, useParams } from "react-router";
+import { useNotes } from "../context/NoteContext";
 
-export default function ActionBar() {
+interface Props {
+  type: "active" | "archived";
+}
+
+export default function ActionBar({ type }: Props) {
+  const { deleteNote, patchNote } = useNotes();
+  const { id } = useParams();
+  const navigate = useNavigate();
+  console.log(id);
+
+  const handleDelete = async () => {
+    await deleteNote(id);
+    navigate("/notes");
+  };
+
+  const handleArchive = async () => {
+    await patchNote(id, true);
+    navigate("/notes");
+  };
+
+  const handleRestore = async () => {
+    await patchNote(id, false);
+    navigate("/notes");
+  };
+
   return (
     <div className="flex justify-between lg:hidden border-b-[2px] p-4 ">
-        <button className="flex">
-          <StepBack /> Go Back
+      <button onClick={() => navigate("/notes")} className="flex">
+        <StepBack /> Go Back
+      </button>
+      <div className="flex gap-4">
+        <button onClick={handleDelete}>
+          <Trash2 />
         </button>
-        <div className="flex gap-4">
-          <button>
-            <Trash2 />
-          </button>
-          <button>
+        {type == "active" ? (
+          <button onClick={handleArchive}>
             <Archive />
           </button>
-          <button className="text-red-500">Cancel</button>
-          <button className="text-[#335CFF]">Save Note</button>
-        </div>
+        ) : (
+          <button onClick={handleRestore}>
+            <RefreshCcw />
+          </button>
+        )}
+      </div>
     </div>
   );
 }
